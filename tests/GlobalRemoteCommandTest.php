06886e569023ac82e83e05c1b51445d3f9d03ac7
Francisco Madeira <xico2k@gmail.com>
2022-10-28 11:56:41 +0100

    test: Add test to `GlobalRemoteCommand`.

---
+++ tests/GlobalRemoteCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Commands\GlobalRemoteCommand;
+use App\Support\ConfigRepository;
+use Spatie\Remote\Commands\RemoteCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+
+it('runs the remote command for an host', function () {
+    (new ConfigRepository)->setHost('default', [
+        'host' => 'example.com',
+        'user' => 'root',
+        'port' => 22,
+        'path' => '/',
+    ]);
+
+    $mock = Mockery::mock(RemoteCommand::class);
+    $this->swap(RemoteCommand::class, $mock);
+
+    $mock
+        ->shouldIgnoreMissing()
+        ->shouldReceive('run')
+        ->once()
+        ->withArgs(fn ($input) =>
+            $input->getParameterOption('rawCommand') === 'test' &&
+            $input->getParameterOption('--host') === 'default'
+        );
+
+    $this->artisan(GlobalRemoteCommand::class, [
+        'rawCommand' => 'test',
+        '--host' => 'default',
+    ]);
+
+});
