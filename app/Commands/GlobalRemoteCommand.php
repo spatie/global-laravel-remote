67a2ff049902fdfb4e51dffdf23642bd191fb8aa
Francisco Madeira <xico2k@gmail.com>
2022-10-28 01:26:20 +0100

    feat: Add ways to choose the different hosts.

--- app/Commands/GlobalRemoteCommand.php
+++ app/Commands/GlobalRemoteCommand.php
@@ -13,19 +13,17 @@ class GlobalRemoteCommand extends Command
     public function handle(): int
     {
         $command = $this->argument('rawCommand');
-        $host = $this->option('host') ?? config('remote.default_host');
+        $host = $this->option('host');
 
-        if (! ($this->config->hosts[$host] ?? null)) {
-            $this->components->warn('Host does not exist: '.$host);
-
-            if (! $this->components->confirm('Would you like to create it?')) {
-                return self::FAILURE;
-            }
-
-            $this->createHost($host);
+        if (! $host) {
+            $host = $this->selectFromHosts() ?? $this->promptToCreate();
         }
 
-        config()->set('remote.hosts', $this->config->hosts);
+        if (! ($this->config->{$host} ?? null)) {
+            $host = $this->promptToCreateNew($host);
+        }
+
+        config()->set('remote.hosts', $this->config->all());
 
         return $this->call(RemoteCommand::class, [
             'rawCommand' => $command,
@@ -35,9 +33,50 @@ class GlobalRemoteCommand extends Command
         ]);
     }
 
-    protected function createHost(string $name): void
+    protected function promptToCreate(): string|false
     {
-        $host = $this->ask('Host?', 'localhost');
+        $this->components->warn('There are no hosts created');
+
+        if (! $this->components->confirm('Would you like to create one?')) {
+            return false;
+        }
+
+        return $this->createHost();
+    }
+
+    protected function promptToCreateNew(): string|false
+    {
+        $this->components->warn('Host does not exist: '.$host);
+
+        if (! $this->components->confirm('Would you like to create it?')) {
+            return false;
+        }
+
+        return $this->createHost($host);
+    }
+
+    protected function selectFromHosts(): string|false
+    {
+        if (count($this->config->all()) === 0) {
+            return false;
+        }
+
+        return $this->components->choice(
+            'Please select one of the available hosts',
+            array_map(
+                fn ($host) => $host['user'].'@'.$host['host'].':'.$host['path'],
+                $this->config->all()
+            )
+        );
+    }
+
+    protected function createHost(?string $name = null): string
+    {
+        if (! $name) {
+            $name = $this->ask('Alias?');
+        }
+
+        $host = $this->ask('Host?', $name);
         $user = $this->ask('User?', 'root');
         $port = $this->ask('Port?', '22');
         $path = $this->ask('Path?', '/');
@@ -48,5 +87,7 @@ class GlobalRemoteCommand extends Command
             'user' => $user,
             'path' => $path,
         ]);
+
+        return $name;
     }
 }
